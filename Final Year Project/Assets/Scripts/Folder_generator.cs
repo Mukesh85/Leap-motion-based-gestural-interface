67a72c7a4c90b273df43f9a6016e380cc0b087e7
mukesh85 <mukesh85tek@gmail.com>
2015-10-18 20:25:41 +0530

    Initial Commit

---
+++ Final Year Project/Assets/Scripts/Folder_generator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+public class Folder_generator : MonoBehaviour {
+	public GameObject g;
+	private float x, y, z;
+	void Start () {
+		DirectoryInfo d = new DirectoryInfo (@"E:\");
+		x = -15;
+		y = 5;
+		z = 14;
+		foreach (System.IO.DirectoryInfo a in d.GetDirectories("*")) 
+		{
+			string name = a.Name;
+			Debug.Log(name);
+			g.name = name;
+			Instantiate(g,new Vector3(x, y, z),transform.rotation);
+			x += 1;
+			z += 0.5f;
+		}
+	}
+
+
+}
